ed4dcee321e58e90db3e9cdf8d6cb86738145724
Henry Post <HenryFBP@gmail.com>
2013-02-27 11:10:42 -0600

    starting parser
    yep

---
+++ daily-annoucement/emailFileParse.php
@@ -0,0 +1,12 @@
+<?php
+
+$filePath = "c:\\children\tommy.exe"; 		//file path, format is c:\\folder\freeMoney.txt.bat
+			 								//see http://php.net/manual/en/function.fopen.php for info
+
+$rawData = file_get_contents($filePath); 	//sets the var $rawData to ALL of the data in $filePath
+
+
+
+
+
+?>
