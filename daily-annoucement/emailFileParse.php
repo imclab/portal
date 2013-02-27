66cef51f2a7ecae24637a5f6663fdcbb6b4623a6
Henry Post <HenryFBP@gmail.com>
2013-02-27 11:18:03 -0600

    more parser
    also documentation of how to recreate it

--- daily-annoucement/emailFileParse.php
+++ daily-annoucement/emailFileParse.php
@@ -1,11 +1,16 @@
 <?php
 
+//see http://php.net/manual/en/ for info
+
 $filePath = "c:\\children\tommy.exe"; 		//file path, format is c:\\folder\freeMoney.txt.bat
-			 								//see http://php.net/manual/en/function.fopen.php for info
+$rawDataDelimiter = "\n";					//what to use to cut one string out from another into diff. rows
+											//i.e. if we see the string HELLO.I.AM.SEXY and
+											//we use "." as the delimiter, the array will be
+											//["HELLO","I","AM","SEXY"]	
 
 $rawData = file_get_contents($filePath); 	//sets the var $rawData to ALL of the data in $filePath
 
-
+$rawDataRows = explode($rawDataDelimiter,$rawData); //format is (what to use for a new row,what string)
 
 
 
