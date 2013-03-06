abee2c7b9e36495eb358b865e610a75ea161eb29
Henry Post <HenryFBP@gmail.com>
2013-03-06 13:34:46 -0600

    more function documentation
    YEP

--- daily-annoucement/emailFileParse.php
+++ daily-annoucement/emailFileParse.php
@@ -11,23 +11,37 @@ $rawData = trim($rawData);								//takes whitespace out from before+after strin
 $rawDataArray = explode("\n",$rawData);					//makes an array with each col a new line
 
 
-function arrayDelBefore($array,$string)				//a function for doing the below things
-{
+/*///
+|	So, the below function will:
+|	1) 	Take in an array with the column of 1 then 2 (up until the length of $array)
+|		and find where the first instance of the $string is
+|
+|	2)	Where that is, 
+|	
+*////
 
+function arrayDelBefore($array,$string)
+{
 	for($arCol = 0; $arCol <= count($array); $arCol++)	//for how long $rawDataArray is, do {stuff}
 	{													//and add 1 to $arCol
-			
-			if(strpos($array($arCol),$string) == true)	// if you find $string in the (1 thru N)
-			{															// then (...)
-			
-				for($i = 0; $i <= $array($arCol); $i++)		//from 0 to where the useless string is, do {stuff}
-				{														//and then plus 1 to $arCol
-					array_fill($arCol, 1, "");							//fill with blankness from $arCol and do it 1x
-				}
-			
+	
+		if(strpos($array($arCol),$string) == true)	// if you find $string in the (1 thru N)
+		{											// then (...)
+		
+			for($i = 0; $i <= $array($arCol); $i++)		//from 0 to where the useless string is, do {stuff}
+			{														//and then plus 1 to $arCol
+				array_fill($arCol, 1, "");							//fill with blankness from $arCol and do it 1x
 			}
+			
+		}
+
 	}
 
 }
+
+
+
+
+
 
 ?>
