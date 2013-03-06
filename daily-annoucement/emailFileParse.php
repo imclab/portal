23b9654aa9d7f0fd6ec3314145b3700c165478c4
Henry Post <HenryFBP@gmail.com>
2013-03-06 12:38:35 -0600

    finished function for prelim parsing
    finds a string and then for every entry in the array before the string
    and the string itself,
    
    set the row to ""

--- daily-annoucement/emailFileParse.php
+++ daily-annoucement/emailFileParse.php
@@ -3,7 +3,7 @@
 $filePath = "normal-email-exampleformatting.txt"; 		//file path, format is c:\\folder\freeMoney.txt.bat
 								
 $rawData = file_get_contents($filePath); 				//sets the var $rawData to ALL of the data in $filePath
-														//can also be used to grab links like http://www.exe.ru/
+														//can also be used to grab links like http://www.exe.ru/lol.html
 
 $rawData = trim($rawData);								//takes whitespace out from before+after string
 
@@ -11,15 +11,16 @@ $rawData = trim($rawData);								//takes whitespace out from before+after strin
 $rawDataArray = explode("\n",$rawData);					//makes an array with each col a new line
 
 for($arrayColNum = 0; $arrayColNum <= count($rawDataArray); $arrayColNum++) //for how long $rawDataArray is, do {stuff}
-{
+{																			//and add 1 to $arrayColNum
+		
 		if(strpos($rawDataArray($arrayColNum),"Content-Type: ") == true)	// if you find "Content-Type: " in the (1 thru N)
 		{																	// then (...)
 		
-			for($i = 0; $i <= $rawDataArray($arrayColNum); $i++)		//from 0 to where in the
-			{															//found useless string,
-																		//do {stuff}
+			for($i = 0; $i <= $rawDataArray($arrayColNum); $i++)		//from 0 to where the useless string is, do {stuff}
+			{															//and then plus 1 to $arrayColNum
+				array_fill($arrayColNum, 1, "");							//fill with blankness from $arrayColNum and do it 1x
 			}
-			
+		
 		}
 }
 
