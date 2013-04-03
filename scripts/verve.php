5d7db3d736924728f841514ed5004bc761b24d03
Henry Post <HenryFBP@gmail.com>
2013-04-03 11:49:04 -0500

    tag stripper works, making 'organizer' (sort of) to make it easier to parse

--- scripts/verve.php
+++ scripts/verve.php
@@ -4,6 +4,13 @@
 / File for getting information
 / from the verve site. Henry Post.
 / 
+/ Here's a note about my stripping HTML tags:
+/  |Only do it AAAAFFFTTTEEEERRRR taking out/organizing
+/  |other useless tags.
+/  +--
+/ 
+/ 
+/ 
 / 
 / 
 / 
@@ -16,15 +23,18 @@
 $filepath = "verve-feed.txt"; 	// filepath can also be a website, needs format ending with a filename,
 								// http://www.google.com/ is not acceptable while http:/www.cats.com/index.html is a-ok
 
-function stripHTMLTags(fileName)
+function stripHTMLTags($fileName)
 {
-	$fileDataRaw = file_get_contents(fileName); //	turns arg to string
+	$fileDataRaw = file_get_contents($fileName);   // turns arg to string
 	
 	$fileDataStripped = strip_tags($fileDataRaw); // strips all html tags (just from <tag> blah </tag> you'll get blah)
 	
 	return $fileDataStripped;
 }
 
+
+//	active code goes below, for testing primarily
+
 echo stripHTMLTags($filepath);
 
-?>
+?>	
