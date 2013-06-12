6d75cf0cff02af658238c8d18869bf373035f65c
Safia Abdalla <safia.lambda@gmail.com>
2013-06-12 17:28:22 +0000

    Removed unnecsssary files

--- site/henry-canvas-drawtest/index.js
+++
@@ -1,7 +0,0 @@
-var widthCircle = 500;
-var heightCircle = 500;
-var spaceCircles = 100;
-
-var circleAvailX = ceil(window.width / widthCircle); //how many circles on X
-var circlaAvailY = ceil(window.height / heightCircle); //how many circles on Y
-
